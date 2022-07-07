c4f7224eaa452bbcb0035311f1e7bc16dabb843f
Branlice <106362981+branlice@users.noreply.github.com>
2022-07-08 02:05:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <h1>测试Vue3的部署</h1>
-  <span>应该会触发自动部署</span>
+  <span>项目由GitPush和手动构建</span>
   
-  20220708 01:41
-  {{ new Date() }}
+  最新稳定版本时间：{{ new Date() }}
   
-  <HelloWorld msg="Hello Vue3" />
+  <b>服务器选材为：</b>
+  
+  <img src="../server.png" href="" />
+  
 </template>
 
 <style>
